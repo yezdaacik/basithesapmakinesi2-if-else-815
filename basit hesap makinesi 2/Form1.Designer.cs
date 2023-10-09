3a15fd78929e10f0e9098a3477a486c293ba8818
yezdanuracik <145451081+yezdanuracik@users.noreply.github.com>
2023-10-09 14:47:39 +0300

    Add files via upload

---
+++ basit hesap makinesi 2/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace basit_hesap_makinesi_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.rbTopla = new System.Windows.Forms.RadioButton();
+            this.rbCikar = new System.Windows.Forms.RadioButton();
+            this.rbCarp = new System.Windows.Forms.RadioButton();
+            this.rbBol = new System.Windows.Forms.RadioButton();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sayı 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sayı 2";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Location = new System.Drawing.Point(29, 224);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(44, 13);
+            this.lblSonuc.TabIndex = 2;
+            this.lblSonuc.Text = "Sonuç :";
+            // 
+            // rbTopla
+            // 
+            this.rbTopla.AutoSize = true;
+            this.rbTopla.Location = new System.Drawing.Point(32, 133);
+            this.rbTopla.Name = "rbTopla";
+            this.rbTopla.Size = new System.Drawing.Size(52, 17);
+            this.rbTopla.TabIndex = 3;
+            this.rbTopla.TabStop = true;
+            this.rbTopla.Text = "Topla";
+            this.rbTopla.UseVisualStyleBackColor = true;
+            // 
+            // rbCikar
+            // 
+            this.rbCikar.AutoSize = true;
+            this.rbCikar.Location = new System.Drawing.Point(134, 133);
+            this.rbCikar.Name = "rbCikar";
+            this.rbCikar.Size = new System.Drawing.Size(49, 17);
+            this.rbCikar.TabIndex = 4;
+            this.rbCikar.TabStop = true;
+            this.rbCikar.Text = "Çıkar";
+            this.rbCikar.UseVisualStyleBackColor = true;
+            // 
+            // rbCarp
+            // 
+            this.rbCarp.AutoSize = true;
+            this.rbCarp.Location = new System.Drawing.Point(32, 156);
+            this.rbCarp.Name = "rbCarp";
+            this.rbCarp.Size = new System.Drawing.Size(47, 17);
+            this.rbCarp.TabIndex = 5;
+            this.rbCarp.TabStop = true;
+            this.rbCarp.Text = "Çarp";
+            this.rbCarp.UseVisualStyleBackColor = true;
+            // 
+            // rbBol
+            // 
+            this.rbBol.AutoSize = true;
+            this.rbBol.Location = new System.Drawing.Point(134, 156);
+            this.rbBol.Name = "rbBol";
+            this.rbBol.Size = new System.Drawing.Size(40, 17);
+            this.rbBol.TabIndex = 6;
+            this.rbBol.TabStop = true;
+            this.rbBol.Text = "Böl";
+            this.rbBol.UseVisualStyleBackColor = true;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(32, 179);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(100, 32);
+            this.btnHesapla.TabIndex = 7;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(98, 66);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(89, 20);
+            this.txtSayi1.TabIndex = 8;
+            this.txtSayi1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(98, 94);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(89, 20);
+            this.txtSayi2.TabIndex = 9;
+            this.txtSayi2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 284);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.rbBol);
+            this.Controls.Add(this.rbCarp);
+            this.Controls.Add(this.rbCikar);
+            this.Controls.Add(this.rbTopla);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.RadioButton rbTopla;
+        private System.Windows.Forms.RadioButton rbCikar;
+        private System.Windows.Forms.RadioButton rbCarp;
+        private System.Windows.Forms.RadioButton rbBol;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+    }
+}
+
---
+++ basit hesap makinesi 2/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace basit_hesap_makinesi_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            float sayi1 = Convert.ToSingle(txtSayi1.Text);
+            float sayi2 = Convert.ToSingle(txtSayi2.Text);
+            float sonuc = 0;
+
+            if (rbTopla.Checked)
+            {
+                sonuc = sayi1 + sayi2;
+            }
+
+            else if (rbCikar.Checked)
+            {
+                sonuc = sayi1 - sayi2;
+            }
+            
+            else if (rbCarp.Checked)
+            {
+                sonuc = -sayi1 * sayi2;
+            }
+
+            else if (rbBol.Checked)
+            {
+                sonuc = sayi1 / sayi2;
+            }
+
+            lblSonuc.Text= "Sonuç: :" + sonuc.ToString();
+        }
+    }
+}
